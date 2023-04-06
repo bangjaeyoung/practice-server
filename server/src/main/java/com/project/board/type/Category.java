3669a05e4bd0e8d484a0c3de6018677aaacf4485
bangjaeyoung <qkdwo4613@gmail.com>
2023-04-06 22:30:27 +0900

    Feat : 게시판 카테고리 추가

---
+++ server/src/main/java/com/project/board/type/Category.java
@@ -0,0 +1,15 @@
+package com.project.board.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Category {
+    LIFE("일상"),
+    MEDICAL("의학"),
+    SHOPPING("쇼핑"),
+    ETC("기타");
+
+    private final String description;
+}
